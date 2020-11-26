5b09a6e7e676b73eb702a5f4fd4acc8a3ab8fe01
xkr-xx <xkrgidwjd916@pusan.ac.kr>
2020-11-26 20:13:20 +0900

    헬스장 페이지상세보기  추가

---
+++ server/dist/resource/auth/entities/testUser.entity.d.ts
@@ -0,0 +1,6 @@
+export declare class TestUser {
+    index: number;
+    id: string;
+    pw: string;
+    email: string;
+}
---
+++ server/dist/resource/test/test.service.d.ts
@@ -0,0 +1,7 @@
+import { Repository } from 'typeorm';
+import { NewTest } from './entities/test.entity';
+export declare class TestService {
+    private readonly newTestRepository;
+    constructor(newTestRepository: Repository<NewTest>);
+    find(): Promise<any>;
+}
